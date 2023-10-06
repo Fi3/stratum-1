stratum-mining/stratum

Update noise algoritm
Updated the SRI encryption and authentication algoritm with a different
flavore of noise. It:
* replace enterly the noise_sv2 crate
* update codec_sv2 to use the new noise and remove the possibility to
  have fragmented noise frames (now a sv2 frame can not be split between
  more sv2 frame that means that sv2 frame max size is 2^24)
* update netwrok_helpers to use the new noise, and improve it that
  change the library API so all the roles need to be updated
* update all the examples, tests and roles to use the improved netwrok_helpers
* update the MG to use the improved netwrok_helpers
* updated all the test and config file to use the new format for private
  and public keys.

roles/jd-client/src/main.rs
@@ -281,12 +281,7 @@ async fn initialize_jd(
     let port_jd = parts.next().unwrap().parse::<u16>().unwrap();
     let jd = JobDeclarator::new(
         SocketAddr::new(IpAddr::from_str(ip_jd.as_str()).unwrap(), port_jd),
-        proxy_config
-            .upstream_authority_pubkey
-            .clone()
-            .into_inner()
-            .as_bytes()
-            .to_owned(),
+        proxy_config.upstream_authority_pubkey.clone().into_bytes(),
         proxy_config.clone(),
         upstream,
         task_collector.clone(),

Fi3/stratum-1

roles/jd-client/src/main.rs
@@ -418,12 +418,7 @@ async fn initialize_jd(
     let port_jd = parts.next().unwrap().parse::<u16>().unwrap();
     let jd = JobDeclarator::new(
         SocketAddr::new(IpAddr::from_str(ip_jd.as_str()).unwrap(), port_jd),
-        upstream_config
-            .authority_pubkey
-            .clone()
-            .into_inner()
-            .as_bytes()
-            .to_owned(),
+        upstream_config.authority_pubkey.clone().into_bytes(),
         proxy_config.clone(),
         upstream,
         task_collector.clone(),